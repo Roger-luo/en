2d33c1e036d23e3a1c92ae6877ce0df061e38ea5
Roger-luo <hiroger@qq.com>
2016-11-09 20:58:00 +0800

    Site updated: 2016-11-09 20:58:00

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,13 +2,15 @@
     // When to show the scroll link
     // higher number = scroll link appears further down the page   
     var upperLimit = 1000;
+    var tocupperLimit = 0;
     var downLimit = 400;
     var doc = document;
     var win = window;
 
     // Our scroll link element
     var scrollElem = $('#totop');
-    var contentElem = $('#')
+    var sidebar = $('#sidebar');
+    var sidetoc = $('#side-toc');
    
     // Scroll to top speed
     var scrollSpeed = 500;
@@ -24,6 +26,19 @@
             $(scrollElem).stop().fadeTo(300, 1);
         }else{       
             $(scrollElem).stop().fadeTo(300, 0); // fade out
+        }
+    });
+
+    sidetoc.hide();
+    $(window).scroll(function () {            
+        var scrollTop = $(document).scrollTop();
+        var scrollBot = $(document).height()-$(window).height()-scrollTop;       
+        if ( scrollBot < downLimit ) {
+            $(sidetoc).stop().fadeTo(300, 0);
+        }else if (scrollTop > tocupperLimit){
+            $(sidetoc).stop().fadeTo(300, 1);
+        }else{       
+            $(sidetoc).stop().fadeTo(300, 0); // fade out
         }
     });
 
